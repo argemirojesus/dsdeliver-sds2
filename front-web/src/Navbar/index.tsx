59491ff2c1506e74e83645bcf61876fd78a12c6e
argemirojesus <argemiro.jesus@gmail.com>
2021-01-10 02:06:17 -0300

    Finish front-web

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -1,17 +1,18 @@
 import './style.css';
-import {ReactComponent as Logo } from './logo.svg';
+import { ReactComponent as Logo } from './logo.svg';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar(){
-return(
-    <nav className="main-navbar">
-        
-        <Logo />
-        <a href="home" className="logo-text">DS Delivery</a>
+function Navbar() {
+    return (
+        <nav className="main-navbar">
 
-    </nav>
-)
+            <Logo />
+            <Link to="/" className="logo-text">DS Delivery</Link>
+
+        </nav>
+    )
 
 }
-
 
 export default Navbar;
