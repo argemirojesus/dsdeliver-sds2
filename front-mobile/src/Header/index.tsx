739f3e98dd0d1e1a3c0cf38a592034bcf423406f
argemirojesus <argemiro.jesus@gmail.com>
2021-01-10 23:55:33 -0300

    Add orders list

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,22 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 function Header() {
+const navigation = useNavigation();
+
+const handleOnPress = () =>{
+  navigation.navigate('Home');
+}
+
   return (
+    <TouchableWithoutFeedback onPress={handleOnPress}>
     <View style={styles.container}>
         <Image source={require('../assets/logo.png')} />
       <Text style={styles.text}>DS Delivery</Text>
      </View>
+     </TouchableWithoutFeedback>
   );
 }
 
@@ -23,7 +33,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     lineHeight: 25,
-    letterSpacing: 0.24,
+    letterSpacing: -0.24,
     color: '#FFF',
     marginLeft: 15,
     fontFamily: 'OpenSans_700Bold'
